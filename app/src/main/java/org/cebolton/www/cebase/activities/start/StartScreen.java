30ee59a6a9ce4b0285823111db1448e63951bcff
Max Echendu <nstinc.it.test@gmail.com>
2018-10-27 00:09:20 +0100

    opening screen build started

---
+++ app/src/main/java/org/cebolton/www/cebase/activities/start/StartScreen.java
@@ -0,0 +1,14 @@
+package org.cebolton.www.cebase.activities.start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.cebolton.www.cebase.R;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+    }
+}
