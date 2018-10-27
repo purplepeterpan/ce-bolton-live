70e5b6b6966f1947dc6273ff449419c2306bbf8b
Max Echendu <nstinc.it.test@gmail.com>
2018-10-27 13:28:46 +0100

    added log in and register page

---
+++ app/src/main/java/org/cebolton/www/cebase/activities/loginregister/LogInRegisterScreen.java
@@ -0,0 +1,14 @@
+package org.cebolton.www.cebase.activities.loginregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.cebolton.www.cebase.R;
+
+public class LogInRegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_register_screen);
+    }
+}
--- app/src/main/java/org/cebolton/www/cebase/activities/start/StartScreen.java
+++ app/src/main/java/org/cebolton/www/cebase/activities/start/StartScreen.java
@@ -1,8 +1,11 @@
 package org.cebolton.www.cebase.activities.start;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import org.cebolton.www.cebase.R;
+import org.cebolton.www.cebase.activities.loginregister.LogInRegisterScreen;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartScreen.this, LogInRegisterScreen.class);
+                startActivity(i);
+            }
+        }, 3500);
     }
 }
